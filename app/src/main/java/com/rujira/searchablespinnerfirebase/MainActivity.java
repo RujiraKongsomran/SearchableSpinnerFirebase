6b0cf6c1f0e3ad59bca2581bd146dabfc2f5358c
Rujira Kongsomran <biwberry@outlook.com>
2019-12-22 11:33:59 +0700

    Add Searchable Spinner to your ActivityMain

--- app/src/main/java/com/rujira/searchablespinnerfirebase/MainActivity.java
+++ app/src/main/java/com/rujira/searchablespinnerfirebase/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
+
 public class MainActivity extends AppCompatActivity {
+
+    SearchableSpinner searchableSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchableSpinner = (SearchableSpinner) findViewById(R.id.searchableSpinner);
+
     }
 }
