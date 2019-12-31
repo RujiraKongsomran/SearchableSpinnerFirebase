ade455fb392af2245d48a376fb9edd6fcd0c6ed2
Rujira Kongsomran <biwberry@outlook.com>
2019-12-31 19:36:08 +0700

    Use BottomSheetDialog to show content

--- app/src/main/java/com/rujira/searchablespinnerfirebase/MainActivity.java
+++ app/src/main/java/com/rujira/searchablespinnerfirebase/MainActivity.java
@@ -4,8 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -13,6 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.rujira.searchablespinnerfirebase.Interface.IFirebaseLoadDone;
 import com.rujira.searchablespinnerfirebase.Model.Movie;
+import com.squareup.picasso.Picasso;
 import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
 
 import java.util.ArrayList;
@@ -25,13 +33,63 @@ public class MainActivity extends AppCompatActivity implements IFirebaseLoadDone
     DatabaseReference movieRef;
     IFirebaseLoadDone iFirebaseLoadDone;
     List<Movie> movies;
+    BottomSheetDialog bottomSheetDialog;
+
+    // View
+    TextView movieTitle, movieDescription;
+    ImageView movieImage;
+    FloatingActionButton btnFav;
+
+    boolean isFirstTimeClick = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Init Dialog
+        bottomSheetDialog = new BottomSheetDialog(this);
+        View bottom_sheet_dialog = getLayoutInflater().inflate(R.layout.layout_movie, null);
+        movieDescription = (TextView) bottom_sheet_dialog.findViewById(R.id.movieDescription);
+        movieTitle = (TextView) bottom_sheet_dialog.findViewById(R.id.movieTitle);
+        movieImage = (ImageView) bottom_sheet_dialog.findViewById(R.id.movieImage);
+        btnFav = (FloatingActionButton) bottom_sheet_dialog.findViewById(R.id.btnFav);
+
+        // Event
+        btnFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Close bottomSheet
+                bottomSheetDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Add Fav !!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Set content
+        bottomSheetDialog.setContentView(bottom_sheet_dialog);
+
         searchableSpinner = (SearchableSpinner) findViewById(R.id.searchableSpinner);
+        searchableSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (!isFirstTimeClick) {
+                    Movie movie = movies.get(i);
+                    movieTitle.setText(movie.getName());
+                    movieDescription.setText(movie.getDescription());
+                    Picasso.get().load(movie.getImage()).into(movieImage);
+
+                    bottomSheetDialog.show();
+                } else {
+                    isFirstTimeClick = false;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         // Init DB
         movieRef = FirebaseDatabase.getInstance().getReference("Movies");
